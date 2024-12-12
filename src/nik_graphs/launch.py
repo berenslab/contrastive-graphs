74a40c730bddcd4d11097aa239a7f16cf60a0484
Niklas Böhm <mail@jnboehm.com>
2024-12-12 11:22:13 +0100

    Add flags to launch.py

--- src/nik_graphs/launch.py
+++ src/nik_graphs/launch.py
@@ -1,14 +1,26 @@
+import argparse
 import importlib
-import sys
+from pathlib import Path
 
 # import nik_graphs
 
 
 def main():
-    print(sys.argv)
 
-    mod = importlib.import_module(".mnist", package="nik_graphs")
-    mod.run_path("", "outfile_unused")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", required=True, type=Path)
+    parser.add_argument("--outfile", required=True, type=Path)
+    args = parser.parse_args()
+
+    path = args.path
+    outfile = args.outfile
+
+    modulename = path.name.split(",")[0]
+
+    mod = importlib.import_module(
+        f".modules.{modulename}", package="nik_graphs"
+    )
+    mod.run_path(path, outfile)
 
 
 if __name__ == "__main__":
