8a54061a4fb282fbc78f29670d31e494e9caed35
Niklas Böhm <mail@jnboehm.com>
2024-12-12 15:54:18 +0100

    Write functionality for saving datasets

---
+++ src/nik_graphs/graph_utils.py
@@ -0,0 +1,59 @@
+import zipfile
+
+import annoy
+import numpy as np
+from scipy import sparse
+
+
+def make_adj_mat(
+    X,
+    n_neighbors=15,
+    metric="euclidean",
+    n_trees=50,
+    seed=None,
+    use_dists=False,
+    symmetrize=True,
+    drop_first=True,
+    n_jobs=-1,
+):
+    t = annoy.AnnoyIndex(X.shape[1], metric)
+    if seed is not None:
+        t.set_seed(seed)
+
+    [t.add_item(i, x) for i, x in enumerate(X)]
+    t.build(n_trees, n_jobs=n_jobs)
+
+    # construct the adjacency matrix for the graph
+    adj = sparse.lil_matrix((X.shape[0], X.shape[0]))
+    for i in range(X.shape[0]):
+        neighs_, dists_ = t.get_nns_by_item(
+            i, n_neighbors + 1, include_distances=True
+        )
+        if drop_first:
+            neighs = neighs_[1:]
+            dists = dists_[1:]
+        else:
+            neighs = neighs_[:n_neighbors]
+            dists = dists_[:n_neighbors]
+
+        adj[i, neighs] = dists if use_dists else 1
+        if symmetrize:
+            adj[neighs, i] = dists if use_dists else 1  # symmetrize on the fly
+
+    return adj, t
+
+
+def save_graph(outfile, adjacency_mat, features, labels):
+    # Will save the adjacency matrix `adj` in the outfile directly.
+    # This way it can be loaded from the zip archive without first
+    # opening the zip file and then opening the file pointer within
+    # the archive.
+    with open(outfile, "xb") as f:
+        sparse.save_npz(f, adjacency_mat.tocsr())
+
+    # need write mode "a" because we have stored `adj` already
+    with zipfile.ZipFile(outfile, "a") as zf:
+        with zf.open("features.npy", "w") as f:
+            np.save(f, features)
+        with zf.open("labels.npy", "w") as f:
+            np.save(f, labels)
--- src/nik_graphs/modules/mnist.py
+++ src/nik_graphs/modules/mnist.py
@@ -1,8 +1,25 @@
+import inspect
 from pathlib import Path
+
+from sklearn import datasets
+
+from ..graph_utils import make_adj_mat, save_graph
 
 
 def run_path(p, outfile):
-    p = Path(p)
-    fpath = Path(__file__).resolve()
 
-    print("Hello from ", fpath, f":\t{p = }")
+    # disrespect XDG cache dir
+    cache_dir = Path.home() / ".cache/scikit_learn_data"
+    mnist = datasets.fetch_openml("mnist_784", data_home=cache_dir)
+
+    adj, annoy_index = make_adj_mat(mnist["data"].values)
+    features = mnist["data"].values
+    labels = mnist["target"].cat.codes.values
+    # will not use `G` here because it's derived from the adjacency matrix
+    # G = nx.from_scipy_sparse_array(adj).to_undirected()
+
+    assert not Path(outfile).exists(), f"{outfile} must not exist."
+    save_graph(outfile, adj, features, labels)
+
+    with open(p / "files.dep", "a") as f:
+        [f.write(inspect.getfile(x) + "\n") for x in [save_graph, datasets]]
