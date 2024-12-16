b4252baf3d311e4822e2619c6df4dc5b68974fd3
Niklas Böhm <mail@jnboehm.com>
2024-12-16 04:01:43 +0100

    Change telegram_send.py to work with async
    Otherwise I cannot send any messages

--- media/telegram_send.py
+++ media/telegram_send.py
@@ -1,115 +1,113 @@
+import asyncio
 import json
 import subprocess
 import sys
 from pathlib import Path
 
+import telegram
 from PIL.PngImagePlugin import PngImageFile, PngInfo
 
-import telegram
 
+async def send_mp4(fname, bot, chat_id):
+    # how to get video info example taken from
+    # https://github.com/kkroening/ffmpeg-python/blob/master/examples/video_info.py
+    import ffmpeg
 
-class MscTelegram:
-    def __init__(self, token=None, chat_id=None):
-        with open(Path(__file__).parent / "telegram.json") as f:
-            self.rc = json.load(f)
+    probe = ffmpeg.probe(fname)
 
-        self.token = token if token is not None else self.rc["token"]
-        self.chat_id = chat_id if chat_id is not None else self.rc["chat_id"]
-        self.bot = telegram.Bot(token=self.token)
+    video_stream = next(
+        (
+            stream
+            for stream in probe["streams"]
+            if stream["codec_type"] == "video"
+        ),
+        None,
+    )
 
-    def send(self, fname):
-        fname = Path(fname)
-        ext = fname.suffix[1:]
-        if ext == "mp4":
-            return self.send_mp4(fname)
-        elif ext == "png":
-            return self.send_png(fname)
-        elif ext == "txt" or ext == "md":
-            return self.send_txt(fname)
-        else:
-            raise RuntimeError(
-                "File ext '{}' not understood, file {} not sent".format(
-                    ext, fname
-                )
-            )
+    if video_stream is None:
+        print("No video stream found", file=sys.stderr)
+        sys.exit(1)
 
-    def send_mp4(self, fname):
-        # how to get video info example taken from
-        # https://github.com/kkroening/ffmpeg-python/blob/master/examples/video_info.py
-        import ffmpeg
+    width = int(video_stream["width"])
+    height = int(video_stream["height"])
+    duration = float(video_stream["duration"])
 
-        probe = ffmpeg.probe(fname)
+    ffmpeg_proc = subprocess.run(
+        [
+            "ffmpeg",
+            "-i",
+            fname,
+            "-c",
+            "copy",
+            "-map_metadata",
+            "0",
+            "-map_metadata:s:v",
+            "0:s:v",
+            "-f",
+            "ffmetadata",
+            "-",
+        ],
+        encoding="utf-8",
+        stdout=subprocess.PIPE,
+    )
 
-        video_stream = next(
-            (
-                stream
-                for stream in probe["streams"]
-                if stream["codec_type"] == "video"
-            ),
-            None,
+    comment = f"`{fname}`"  # make sure that the variabel exists
+
+    with open(fname, "rb") as f:
+        return bot.send_animation(
+            chat_id=chat_id,
+            animation=f,
+            width=width,
+            height=height,
+            duration=duration,
+            parse_mode="Markdown",
+            caption=comment,
         )
 
-        if video_stream is None:
-            print("No video stream found", file=sys.stderr)
-            sys.exit(1)
 
-        width = int(video_stream["width"])
-        height = int(video_stream["height"])
-        duration = float(video_stream["duration"])
+async def send_png(fname, bot, chat_id):
+    png = PngImageFile(fname)
 
-        ffmpeg_proc = subprocess.run(
-            [
-                "ffmpeg",
-                "-i",
-                fname,
-                "-c",
-                "copy",
-                "-map_metadata",
-                "0",
-                "-map_metadata:s:v",
-                "0:s:v",
-                "-f",
-                "ffmetadata",
-                "-",
-            ],
-            encoding="utf-8",
-            stdout=subprocess.PIPE,
+    png.load()  # load metadata
+    with open(fname, "rb") as f:
+        return await bot.send_photo(
+            chat_id=chat_id,
+            photo=f,
+            parse_mode="Markdown",
+            caption=png.info.get("Comment", "`{}`".format(fname.name)),
         )
 
-        comment = f"`{fname}`"  # make sure that the variabel exists
 
-        with open(fname, "rb") as f:
-            return self.bot.send_animation(
-                chat_id=self.chat_id,
-                animation=f,
-                width=width,
-                height=height,
-                duration=duration,
-                parse_mode="Markdown",
-                caption=comment,
-            )
+async def send_txt(fname, bot, chat_id):
+    with open(fname) as f:
+        text = f.read()
 
-    def send_png(self, fname):
-        png = PngImageFile(fname)
+    return bot.send_message(chat_id=chat_id, text=text, parse_mode="Markdown")
 
-        png.load()  # load metadata
-        with open(fname, "rb") as f:
-            return self.bot.send_photo(
-                chat_id=self.chat_id,
-                photo=f,
-                parse_mode="Markdown",
-                caption=png.info.get("Comment", "`{}`".format(fname.name)),
-            )
 
-    def send_txt(self, fname):
-        with open(fname) as f:
-            text = f.read()
-
-        return self.bot.send_message(
-            chat_id=self.chat_id, text=text, parse_mode="Markdown"
+async def send(fname, bot, chat_id):
+    fname = Path(fname)
+    ext = fname.suffix[1:]
+    if ext == "mp4":
+        return await send_mp4(fname, bot, chat_id)
+    elif ext == "png":
+        return await send_png(fname, bot, chat_id)
+    elif ext == "txt" or ext == "md":
+        return await send_txt(fname, bot, chat_id)
+    else:
+        raise RuntimeError(
+            "File ext '{}' not understood, file {} not sent".format(ext, fname)
         )
+
+
+async def main():
+    with open(Path(__file__).parent / "telegram.json") as f:
+        rc = json.load(f)
+
+    bot = telegram.Bot(token=rc["token"])
+    async with bot:
+        await send(sys.argv[1], bot, rc["chat_id"])
 
 
 if __name__ == "__main__":
-    bot = MscTelegram()
-    bot.send(sys.argv[1])
+    asyncio.run(main())
