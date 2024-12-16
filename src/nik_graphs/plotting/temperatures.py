bf67e6d57c2e920c0fe6f7563a44f35397195483
Niklas Böhm <mail@jnboehm.com>
2024-12-17 00:59:49 +0100

    Remove code creating the df when plotting

--- src/nik_graphs/plotting/temperatures.py
+++ src/nik_graphs/plotting/temperatures.py
@@ -13,73 +13,19 @@ N_EPOCHS = 30
 
 # example plotname = "temperatures"
 def deplist(plotname):
-    depvals = deps(plotname).values()
-    return [f for lis in depvals for f in lis]
-
-
-def deps(plotname):
     # dataset, algo, _name = plotname.name.split(".")
     assert plotname.name == "temperatures"
 
-    dataset = "mnist"
+    path = Path("../dataframes/temp_evals.csv")
 
-    from ..modules.cne import tsimcne_nonparam
-
-    sig = inspect.signature(tsimcne_nonparam)
-    default_temp = sig.parameters["temp"].default
-    default_n_epochs = sig.parameters["n_epochs"].default
-
-    # rng = np.random.default_rng(333**3)
-    # rints = rng.integers(10000, size=2)
-    rstrs = [""]  # + [f",random_state={r}" for r in rints]
-
-    n_epochs = f",n_epochs={N_EPOCHS}" if default_n_epochs != N_EPOCHS else ""
-    path = Path("../runs") / dataset
-    paths = []
-    for temp, rstr in itertools.product(_TEMPERATURES, rstrs):
-        tempstr = f",temp={temp}" if temp != default_temp else ""
-        paths.append(path / ("cne,metric=cosine" + n_epochs + tempstr + rstr))
-
-    depdict = {
-        k: [p / k / "1.zip" for p in paths] for k in ["lin", "knn", "recall"]
-    }
-
-    depdict["srcfiles"] = [
+    return [path] + [
         inspect.getfile(f) for f in [inspect, Path, np, plt, zipfile]
     ]
-    return depdict
 
 
 def plot_path(plotname, outfile, format="pdf"):
-    depd = deps(plotname)
-    depd.pop("srcfiles")
-
-    # labels = np.load(files["data"])["labels"]
-    df_dict = dict()
-    for k, v in depd.items():
-        df_ = pl.DataFrame(dict(temp=[], epoch=[], k=[]))
-        for temp, zipf in zip(_TEMPERATURES, v):
-            with zipfile.ZipFile(zipf) as zf:
-                with zf.open("scores.csv") as f:
-                    df1 = pl.read_csv(f).drop("step")
-
-                with zf.open("score.txt") as f:
-                    score = float(f.read())
-                    df2 = pl.DataFrame(
-                        dict(epoch=[N_EPOCHS - 1], score=[score])
-                    )
-            if df2["epoch"] in df1["epoch"]:
-                df__ = df1
-            else:
-                df__ = df1.vstack(df2)  #
-            df__ = df__.with_columns(pl.lit(temp).alias("temp"))
-            df_ = df_.vstack(df__.rename(dict(score=k))[["temp", "epoch", k]])
-        df_dict[k] = df_
-
-    df = pl.concat(
-        [df_dict[k][["temp", "epoch"]]] + [df[k] for k, df in df_dict.items()],
-        how="horizontal",
-    )
+    deps = deplist(plotname)
+    df = pl.read_csv(deps[0])
 
     return plot(df, outfile=outfile, format=format)
 
