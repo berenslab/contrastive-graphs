d9657e71a2528cd03c2e1d45830be7ffd0d076db
Niklas Böhm <mail@jnboehm.com>
2024-12-19 04:27:52 +0100

    Add tfdp module for graphs

---
+++ src/nik_graphs/modules/tfdp.py
@@ -0,0 +1,52 @@
+import inspect
+import zipfile
+
+import numpy as np
+from scipy import sparse
+
+from tfdp.tfdp import tFDP
+
+from ..path_utils import path_to_kwargs
+from .nx import _get_init
+
+__partition__ = "cpu-galvani"
+
+
+def run_path(path, outfile):
+    zipf = path.parent / "1.zip"
+
+    with open(path / "files.dep", "a") as f:
+        f.write(f"{inspect.getfile(path_to_kwargs)}\n")
+
+    A = sparse.load_npz(zipf)
+
+    name, kwargs = path_to_kwargs(path)
+    assert name == "nx"
+
+    Y = tfdp(A, **kwargs)
+
+    with zipfile.ZipFile(outfile, "a") as zf:
+        with zf.open("embedding.npy", "w") as f:
+            np.save(f, Y)
+
+
+def tfdp(
+    A,
+    n_epochs=100,
+    initialization="spectral",
+    random_state=505**3,
+    **kwargs,
+):
+
+    Y_init = _get_init(A, initialization, 2, random_state)
+    _tfdp = tFDP(
+        init=Y_init, max_iter=n_epochs, randseed=random_state, **kwargs
+    )
+
+    A = A.tocsr()
+    graph = sparse.tril(A)
+    edgesrc = A.indptr
+    edgetgt = A.indices
+
+    Y, _elapsed_time = _tfdp.optmization(graph, edgesrc, edgetgt)
+    return Y
