9e63b4fb07eb281c389c96833b94ae561a7f91e3
Niklas Böhm <mail@jnboehm.com>
2024-12-29 16:14:00 +0100

    Add code to plot edge graphs

--- src/nik_graphs/plotting/layouts.py
+++ src/nik_graphs/plotting/layouts.py
@@ -22,11 +22,13 @@ def plot_path(plotname, outfile, format="pdf"):
 
 
 def plot(h5, outfile, format="pdf"):
+    import matplotlib as mpl
+    import numpy as np
     from matplotlib import pyplot as plt
 
     from ..plot import letter_dict
 
-    letters = iter(string.ascii_lowercase)
+    letters = itertools.cycle(string.ascii_lowercase)
     fig = plt.figure(figsize=(3, 1.1 * len(h5)))
     figs = fig.subfigures(len(h5))
     for sfig, dataset in zip(figs, h5):
@@ -34,12 +36,14 @@ def plot(h5, outfile, format="pdf"):
         h5_ds = h5[dataset]
         keys = [k for k in h5_ds if k not in ["edges", "labels"]]
         labels = h5_ds["labels"]
+        row = h5_ds["edges/row"]
+        col = h5_ds["edges/col"]
 
         axd = sfig.subplot_mosaic([keys])
         for key, ax in axd.items():
             ax.set_title(key)
 
-            data = h5_ds[key]
+            data = np.array(h5_ds[key])
             ax.scatter(data[:, 0], data[:, 1], c=labels, rasterized=True)
             ax.set_title(next(letters), **letter_dict())
             ax.axis("equal")
@@ -60,5 +64,16 @@ def plot(h5, outfile, format="pdf"):
                 ha="right",
                 va="top",
             )
+
+            pts = np.hstack((data[row], data[col])).reshape(len(row), 2, 2)
+            lines = mpl.collections.LineCollection(
+                pts,
+                alpha=0.05,
+                color="xkcd:dark grey",
+                antialiaseds=True,
+                zorder=0.9,
+                rasterized=True,
+            )
+            ax.add_collection(lines)
 
     fig.savefig(outfile, format=format)
