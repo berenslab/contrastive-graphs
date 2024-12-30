62cdd7db27f3210145eaa9b8c1461ba57b3aca0f
Niklas Böhm <mail@jnboehm.com>
2024-12-30 15:57:22 +0100

    Add more plotting code to trainmetrics (final)
    I'm somewhat happy with how it looks now

--- src/nik_graphs/plotting/trainmetrics.py
+++ src/nik_graphs/plotting/trainmetrics.py
@@ -21,12 +21,15 @@ def deps(plotname):
 
 def plot_path(plotname, outfile, format="pdf"):
     import polars as pl
+    import yaml
 
     depdict = deps(plotname)
 
     zpath = zipfile.Path(depdict["embedding"])
     with (zpath / "lightning_logs/metrics.csv").open() as f:
         train_df = pl.read_csv(f)
+    with (zpath / "lightning_logs/hparams.yaml").open() as f:
+        hparams = yaml.safe_load(f)
 
     # labels = np.load(files["data"])["labels"]
     df_dict = dict()
@@ -34,7 +37,7 @@ def plot_path(plotname, outfile, format="pdf"):
         zipf = depdict[k]
         with zipfile.ZipFile(zipf) as zf:
             with zf.open("scores.csv") as f:
-                df1 = pl.read_csv(f).drop("step")
+                df1 = pl.read_csv(f)
 
         df_ = df1.rename(dict(score=k), strict=False)
         df_dict[k] = df_
@@ -44,32 +47,80 @@ def plot_path(plotname, outfile, format="pdf"):
         how="horizontal",
     )
 
-    return plot(train_df, df, outfile=outfile, format=format)
+    return plot(
+        train_df,
+        df,
+        batches_per_epoch=hparams["batches_per_epoch"],
+        outfile=outfile,
+        format=format,
+    )
 
 
-def plot(train_df, df, outfile=None, format="pdf"):
+def plot(train_df, df, batches_per_epoch=None, outfile=None, format="pdf"):
+    import matplotlib as mpl
     import polars as pl
     from matplotlib import pyplot as plt
 
     dfg = train_df.group_by("epoch")
 
+    def plot_steps(ax, k, color):
+        if batches_per_epoch is not None:
+            df = train_df[["step", k]].drop_nulls()
+            s = df[k] if k != "logtemp" else df[k].exp()
+            ax.plot(
+                df["step"] / batches_per_epoch,
+                s,
+                color=color,
+                alpha=0.618,
+            )
+        else:
+            import warnings
+
+            warnings.warn(
+                f"{batches_per_epoch=} so only mean over epoch will be plotted"
+            )
+
+    def set_bounds(ax):
+        if batches_per_epoch is not None:
+            ax.spines.bottom.set_bounds(
+                0, (train_df["step"] / batches_per_epoch).ceil().max()
+            )
+
     # figsize is 3.25 inches, that is a single column in icml 2025 paper format.
     fig, axd = plt.subplot_mosaic(
-        [["plot", "legend"]], figsize=(3.25, 2.5), width_ratios=[1, 0.25]
+        [["plot", "acc"], ["temperature", "legend"]],
+        figsize=(3.25, 2),
     )
-    ax = axd["plot"]
-    for k in ["logtemp", "loss"]:
-        epoch, val = dfg.agg(pl.mean(k)).sort(by="epoch")[["epoch", k]]
-        if k == "logtemp":
-            k = "temperature"
-            val = val.exp()
-        ax.plot(epoch, val, label=k)
+    [ax.sharex(axd["plot"]) for k, ax in axd.items() if k != "plot"]
+    [set_bounds(ax) for k, ax in axd.items()]
 
+    ax = axd["temperature"]
+    epoch, logtemp = dfg.agg(pl.mean("logtemp")).sort(by="epoch")[
+        ["epoch", "logtemp"]
+    ]
+    temp = logtemp.exp()
+    (line,) = ax.plot(epoch, temp, label="temperature", color="xkcd:dark grey")
+    plot_steps(ax, "logtemp", color=line.get_color())
+    ax.set(xlabel="epoch", ylabel="temperature", yscale="log")
+
+    ax = axd["plot"]
+    for k in ["loss"]:
+        epoch, val = dfg.agg(pl.mean(k)).sort(by="epoch")[["epoch", k]]
+        (line,) = ax.plot(epoch, val, label=k)
+        color = line.get_color()
+        plot_steps(ax, k, color)
+
+    ax.legend()
+
+    ax = axd["acc"]
     for k in ["knn", "lin", "recall"]:
         ax.plot(df["epoch"], df[k], label=k)
+    ax.yaxis.set_major_formatter(mpl.ticker.PercentFormatter(1))
+    ax.legend()
+    ax.set(xlabel="epoch", ylim=(0, 1))
 
-    handles, labels = ax.get_legend_handles_labels()
-    axd["legend"].legend(handles=handles, labels=labels, loc="center")
+    # handles, labels = ax.get_legend_handles_labels()
+    # axd["legend"].legend(handles=handles, labels=labels, loc="center")
     axd["legend"].set_axis_off()
 
     fig.savefig(outfile, format=format)
