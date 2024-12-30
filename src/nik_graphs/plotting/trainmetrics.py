1a91354175dfb2c46b66dd85bbf33a2dbeb50a28
Niklas Böhm <mail@jnboehm.com>
2024-12-30 20:19:53 +0100

    Change plot to be more compact and informative

--- src/nik_graphs/plotting/trainmetrics.py
+++ src/nik_graphs/plotting/trainmetrics.py
@@ -15,7 +15,6 @@ def deps(plotname):
     path = Path("../runs") / dataset / "cne,loss=infonce-temp"
     depdict = {k: path / k / "1.zip" for k in ["lin", "knn", "recall"]}
     depdict["embedding"] = path / "1.zip"
-    depdict["data"] = path.parent / "1.zip"
     return depdict
 
 
@@ -31,7 +30,6 @@ def plot_path(plotname, outfile, format="pdf"):
     with (zpath / "lightning_logs/hparams.yaml").open() as f:
         hparams = yaml.safe_load(f)
 
-    # labels = np.load(files["data"])["labels"]
     df_dict = dict()
     for k in ["lin", "knn", "recall"]:
         zipf = depdict[k]
@@ -61,6 +59,8 @@ def plot(train_df, df, batches_per_epoch=None, outfile=None, format="pdf"):
     import polars as pl
     from matplotlib import pyplot as plt
 
+    from ..plot import add_letters
+
     dfg = train_df.group_by("epoch")
 
     def plot_steps(ax, k, color):
@@ -88,11 +88,12 @@ def plot(train_df, df, batches_per_epoch=None, outfile=None, format="pdf"):
 
     # figsize is 3.25 inches, that is a single column in icml 2025 paper format.
     fig, axd = plt.subplot_mosaic(
-        [["plot", "acc"], ["temperature", "legend"]],
-        figsize=(3.25, 2),
+        [["loss", "temperature", "acc"]],
+        figsize=(3.25, 1.1),
     )
-    [ax.sharex(axd["plot"]) for k, ax in axd.items() if k != "plot"]
+    [ax.sharex(axd["loss"]) for k, ax in axd.items() if k != "loss"]
     [set_bounds(ax) for k, ax in axd.items()]
+    add_letters(axd.values())
 
     ax = axd["temperature"]
     epoch, logtemp = dfg.agg(pl.mean("logtemp")).sort(by="epoch")[
@@ -101,26 +102,28 @@ def plot(train_df, df, batches_per_epoch=None, outfile=None, format="pdf"):
     temp = logtemp.exp()
     (line,) = ax.plot(epoch, temp, label="temperature", color="xkcd:dark grey")
     plot_steps(ax, "logtemp", color=line.get_color())
-    ax.set(xlabel="epoch", ylabel="temperature", yscale="log")
+    ax.set(xlabel="epoch", ylabel=r"temperature $\tau$", yscale="log")
+    last_temp = temp.drop_nulls()[-1]
+    ax.text(
+        1,
+        last_temp,
+        rf"$\tau = {last_temp:.1g}$",
+        ha="right",
+        va="bottom",
+        transform=ax.get_yaxis_transform(),
+    )
 
-    ax = axd["plot"]
-    for k in ["loss"]:
-        epoch, val = dfg.agg(pl.mean(k)).sort(by="epoch")[["epoch", k]]
-        (line,) = ax.plot(epoch, val, label=k)
-        color = line.get_color()
-        plot_steps(ax, k, color)
-
-    ax.legend()
+    ax = axd["loss"]
+    epoch, val = dfg.agg(pl.mean("loss")).sort(by="epoch")[["epoch", "loss"]]
+    (line,) = ax.plot(epoch, val, label="loss", color="xkcd:dark grey")
+    plot_steps(ax, "loss", line.get_color())
+    ax.set(xlabel="epoch", ylabel="loss")
 
     ax = axd["acc"]
     for k in ["knn", "lin", "recall"]:
         ax.plot(df["epoch"], df[k], label=k)
     ax.yaxis.set_major_formatter(mpl.ticker.PercentFormatter(1))
     ax.legend()
-    ax.set(xlabel="epoch", ylim=(0, 1))
-
-    # handles, labels = ax.get_legend_handles_labels()
-    # axd["legend"].legend(handles=handles, labels=labels, loc="center")
-    axd["legend"].set_axis_off()
+    ax.set(xlabel="epoch")
 
     fig.savefig(outfile, format=format)
