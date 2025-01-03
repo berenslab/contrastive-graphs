e1e831aed62aa1bff52d410b4874bebcdd225ee8
Niklas Böhm <mail@jnboehm.com>
2025-01-03 14:36:57 +0100

    Change lin.py, to eval it on feature space of orig dataset

--- src/nik_graphs/modules/lin.py
+++ src/nik_graphs/modules/lin.py
@@ -22,11 +22,19 @@ def run_path(path, outfile):
     # assumption: we have the embedding generated in the direct parent
     # and the dataset is defined on directory above.  This should
     # maybe have some code that actually searches the parent dirs, but
-    # so far this holds for all approaches.
+    # so far this holds for all approaches, except for the case where
+    # we evaluate the feature space of the dataset directly.
     embeddings_dir = path.parent
     data_dir = path.parent.parent
+    # if we evaluate the input feature space directly, we change the
+    # data_dir and the corresponding embedding key
+    if data_dir.name == "runs":
+        data_dir = embeddings_dir
+        embedding_key = "features"
+    else:
+        embedding_key = "embedding"
 
-    X = np.load(embeddings_dir / "1.zip")["embedding"]
+    X = np.load(embeddings_dir / "1.zip")[embedding_key]
     npz = np.load(data_dir / "1.zip")
     labels = npz["labels"]
 
