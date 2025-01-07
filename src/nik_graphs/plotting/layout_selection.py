09951401841816f997a24fe666a5b911dce2fa37
Niklas Böhm <mail@jnboehm.com>
2025-01-07 14:08:43 +0100

    Add plot file showing layouts for a given dataset

---
+++ src/nik_graphs/plotting/layout_selection.py
@@ -0,0 +1,83 @@
+from pathlib import Path
+
+
+def deplist(dispatch: Path):
+    return ["../dataframes/all_layouts.h5"]
+
+
+def plot_path(plotname, outfile, format="pdf"):
+    import h5py
+    from matplotlib import pyplot as plt
+
+    dataset, _layout_selection = plotname.name.split(".")
+    assert _layout_selection == "layout_selection"
+
+    h5file = deplist(plotname)[0]
+
+    with h5py.File(h5file) as h5:
+        with plt.rc_context({"font.family": ["Roboto", "Noto Sans"]}):
+            return plot(h5, dataset, outfile=outfile, format=format)
+
+
+def plot(h5, dataset, outfile, format="pdf"):
+    import matplotlib as mpl
+    import numpy as np
+    from matplotlib import pyplot as plt
+    from scipy import linalg
+
+    from ..plot import add_scalebars, letter_dict, letter_iterator
+
+    letters = letter_iterator()
+    ldict = letter_dict()
+    ldict.pop("horizontalalignment", None)
+    keys = ["tsne", "drgraph", "fa2", "tfdp"]
+    fig, axd = plt.subplot_mosaic(
+        np.array(keys).reshape(2, 2), figsize=(3.25, 3)
+    )
+
+    h5_ds = h5[dataset]
+    labels = h5_ds["labels"]
+    row = h5_ds["edges/row"]
+    col = h5_ds["edges/col"]
+    for key, ax in axd.items():
+        anchor = h5_ds["tsne"]  # take any array
+
+        ax.set_title(key)
+
+        data = np.array(h5_ds[key])
+        rot, _scale = linalg.orthogonal_procrustes(data, anchor)
+        data = data @ rot.round(10)
+        ax.scatter(*data.T, c=labels, rasterized=True)
+        ax.set_title(next(letters), **ldict, ha="left")
+        ax.axis("equal")
+        add_scalebars(ax)
+
+        lines = (
+            f"{k} = {v:5.1%}"
+            for k, v in h5_ds[key].attrs.items()
+            if k != "lin"
+        )
+        txt = "\n".join(sorted(lines, key=len, reverse=True))
+        ax.text(
+            1,
+            1,
+            txt,
+            transform=ax.transAxes,
+            fontsize="x-small",
+            family="monospace",
+            ha="right",
+            va="top",
+        )
+
+        pts = np.hstack((data[row], data[col])).reshape(len(row), 2, 2)
+        lines = mpl.collections.LineCollection(
+            pts,
+            alpha=0.05,
+            color="xkcd:dark grey",
+            antialiaseds=True,
+            zorder=0.9,
+            rasterized=True,
+        )
+        ax.add_collection(lines)
+
+    fig.savefig(outfile, format=format)
