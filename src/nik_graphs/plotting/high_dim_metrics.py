fa295135c59c65c2f4a5bf3ead5030ef6c5e7d3b
Niklas Böhm <mail@jnboehm.com>
2025-01-07 17:21:19 +0100

    Change high_dim_metrics to a bar plot

--- src/nik_graphs/plotting/high_dim_metrics.py
+++ src/nik_graphs/plotting/high_dim_metrics.py
@@ -20,36 +20,74 @@ def plot(df_full, outfile, format="pdf"):
 
     from ..plot import add_letters
 
+    n_bars = len(df_full["name"].unique())
+    bar_width = 1 / (n_bars + 1.62)
+
     keys = ["knn", "lin", "recall"]  # , "time"]
     legend = ["legend"] * len(keys)
 
     fig, axd = plt.subplot_mosaic(
-        [keys, legend], figsize=(3.25, 1.1), height_ratios=[1, 0.05]
+        [keys, legend],
+        figsize=(3.25, 1.3),
+        height_ratios=[1, 0.05],
+        sharey=True,
+        constrained_layout=dict(h_pad=0, w_pad=0 / 72),
     )
     ax_legend = axd.pop("legend")
     for key, ax in axd.items():
         df_metric = df_full.group_by(
-            ["dataset", "run_name"], maintain_order=True
+            ["dataset", "name"], maintain_order=True
         ).agg(
-            pl.first("name", "n_edges"),
+            pl.first("n_edges"),
             pl.mean(key).alias("mean"),
             pl.std(key).alias("std"),
         )
-        for (_,), df in df_metric.group_by("run_name", maintain_order=True):
+        for i, ((_,), df) in enumerate(
+            df_metric.group_by("name", maintain_order=True)
+        ):
             df = df.sort(by="n_edges")
-            label = df["name"].head(1).item()
-            x, m, std = df[["n_edges", "mean", "std"]]
+            x, m, std = df.with_row_index()[["index", "mean", "std"]]
             ax.yaxis.set_major_formatter(
                 mpl.ticker.PercentFormatter(1, decimals=0)
             )
-            (line,) = ax.plot(x, m, label=label, marker="o")
-            ax.fill_between(
-                x, m + std, m - std, color=line.get_color(), alpha=0.618
+            ax.bar(x + i * bar_width, m, label=df["name"][0], width=bar_width)
+            ax.errorbar(
+                x + i * bar_width,
+                m,
+                yerr=std,
+                fmt="none",
+                ecolor="xkcd:dark grey",
+                zorder=5,
             )
-        if key == "lin":
-            add_dataset_names(ax, df_metric, key)
-        ax.set(title=key, xscale="log")
+
+        _dftix = (
+            df_full[["dataset", "n_edges"]]
+            .unique()
+            .sort("n_edges")
+            .with_row_index()[["dataset", "index"]]
+        )
+        ax.set_xticks(
+            _dftix["index"] + (bar_width * (n_bars - 1)) / 2,
+            _dftix["dataset"],
+            rotation=45,
+            ha="right",
+            rotation_mode="anchor",
+        )
         ax.set_title(key, family="Roboto")
+        [ax.axhline(y, color="white") for y in [0.25, 0.5, 0.75]]
+        ax.spines.left.set_visible(False)
+        ax.tick_params("both", width=0, length=0, labelsize=6)
+        ax.set_yticks([0, 0.25, 0.5, 0.75])
+        ax.hlines(
+            [0] * len(_dftix),
+            xmin=_dftix["index"] - bar_width / 2,
+            xmax=_dftix["index"] + bar_width * n_bars - bar_width / 2,
+            lw=plt.rcParams["axes.linewidth"],
+            color="black",
+            clip_on=False,
+        )
+        ax.spines.bottom.set_visible(False)
+        ax.margins(0)
 
     handles, labels = ax.get_legend_handles_labels()
     ax_legend.set_axis_off()
@@ -57,7 +95,7 @@ def plot(df_full, outfile, format="pdf"):
         handles=handles,
         labels=labels,
         ncols=len(handles),
-        loc="center right",
+        loc="center",
         borderaxespad=0,
         borderpad=0,
         labelspacing=0,
@@ -66,68 +104,5 @@ def plot(df_full, outfile, format="pdf"):
         handlelength=1.25,
         fontsize=7,
     )
-    [ax.set_xlabel("number of edges") for ax in axd.values()]
     add_letters(axd.values())
     fig.savefig(outfile, format=format)
-
-
-def add_dataset_names(ax, df1, ykey):
-    # loosely based on
-    # https://datascience.stackexchange.com/questions/66506/
-    # what-is-a-good-method-for-detecting-local-minims-and-maxims
-    # sdiff = df1["mean"].diff()
-    # sign_ind = sdiff.tail(-1).sign() - sdiff.head(-1).sign()
-    for (dataset,), df in df1.group_by("dataset"):
-
-        # index, dataset, n_edges, mean, std = row
-        # import sys
-
-        # print(df, file=sys.stderr)
-
-        # all of those alginment and xytext values have been
-        # determined manually by looking at the plot
-        match dataset:
-            case "citeseer":
-                m, std = df[["mean", "std"]].min()
-                y = m - std
-                kwargs = dict(ha="left", va="top", xytext=(0, 0))
-            case "cora":
-                m = df[["mean"]].max()
-                y = m
-                kwargs = dict(ha="left", va="bottom", xytext=(-4, 1.75))
-            case "pubmed":
-                m, std = df[["mean", "std"]].min()
-                y = m
-                kwargs = dict(ha="center", va="top", xytext=(-4, -0.5))
-            case "computer":
-                m, std = df[["mean"]].min(), df[["std"]].max()
-                y = m - std
-                dataset = "comp."
-                kwargs = dict(ha="center", va="top", xytext=(0, 0))
-            case "photo":
-                m, std = df[["mean", "std"]].max()
-                y = m + std
-                kwargs = dict(ha="right", va="center", xytext=(-2.0, 0))
-            case "mnist":
-                m = df[["mean"]].max()
-                y = m
-                kwargs = dict(ha="right", va="baseline", xytext=(-1.5, 0))
-            case "arxiv":
-                m = df[["mean"]].min()
-                y = m
-                kwargs = dict(ha="right", va="baseline", xytext=(-1.5, 0))
-            case _:
-                import warnings
-
-                warnings.warn(f"Unknown {dataset=!r}, default annotation")
-                y = df[["mean"]].mean()
-                kwargs = dict(xytext=(0, 0))
-
-        x = df["n_edges"].head(1).item()
-        ax.annotate(
-            text=dataset,
-            xy=(x, y.item()),
-            **kwargs,
-            textcoords="offset points",
-            fontsize=6,
-        )
