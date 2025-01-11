43ca7743cc2be58050210125446438ecc9039602
Niklas Böhm <mail@jnboehm.com>
2025-01-11 09:38:56 +0100

    Add low_dim_benchmarks.py
    Similar to high_dim_benchmarks

---
+++ src/nik_graphs/aggregate/low_dim_benchmarks.py
@@ -0,0 +1,90 @@
+import itertools
+from pathlib import Path
+
+DATASETS = [
+    "cora",
+    "computer",
+    "photo",
+    "citeseer",
+    "mnist",
+    "arxiv",
+    "pubmed",
+    "mag",
+]
+LAYOUTS = ["tsne", "spectral", "sgtsnepi", "drgraph", "fa2", "tfdp"]
+
+RANDOM_STATES = [None, 1111, 2222]
+
+
+def deplist(dispatch: Path):
+    depvals = deps(dispatch).values()
+    return list(set([f for lis in depvals for f in lis]))
+
+
+def deps(dispatch):
+    # dataset, algo, _name = dispatch.name.split(".")
+    assert str(dispatch) == "low_dim_benchmarks"
+
+    paths = []
+
+    for dataset, mname, r in iterator():
+        # modname = model.split(",")[0]
+
+        path = Path("../runs") / dataset
+        randstr = f",random_state={r}" if r is not None else ""
+        paths.append(path / (mname + randstr))
+
+    depdict = {
+        k: [p / k / "1.zip" for p in paths]
+        for k in [".", "..", "lin", "knn", "recall"]
+    }
+    return depdict
+
+
+def iterator():
+    return itertools.product(DATASETS, LAYOUTS, RANDOM_STATES)
+
+
+def aggregate_path(path, outfile=None):
+    import zipfile
+    from collections import defaultdict
+
+    import numpy as np
+    import polars as pl
+
+    results = defaultdict(list)
+    pathdict = dict()
+    for key, ziplist in deps(path).items():
+        for (dataset, mname, r), zipf in zip(iterator(), ziplist):
+            zpath = zipfile.Path(zipf)
+
+            # "." is the first entry, so here we store all columns
+            if key == ".":
+                results["dataset"].append(dataset)
+                results["name"].append(mname)
+                results["random_state"].append(r)
+                secs = (zpath / "elapsed_secs.txt").read_text()
+                results["time"].append(float(secs))
+            elif key == "..":
+                path = zipf.resolve()
+                if path not in pathdict:
+                    npz = np.load(zipf)
+                    n, m = npz["shape"]
+                    nnz = npz["data"].shape[0]
+                    results["n_pts"].append(n)
+                    results["n_edges"].append(nnz)
+                    pathdict[path] = n, nnz
+                else:
+                    n, nnz = pathdict[path]
+                    results["n_pts"].append(n)
+                    results["n_edges"].append(nnz)
+            else:
+                acctxt = (zpath / "score.txt").read_text()
+                results[key].append(float(acctxt))
+
+    df = pl.DataFrame(results)
+    if outfile is not None:
+        with open(outfile, "xb") as f:
+            df.write_parquet(f)
+
+    return df
