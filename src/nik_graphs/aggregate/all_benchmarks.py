2ae187b3eecafea3fbc192d9c7b570d85eee6995
Niklas Böhm <mail@jnboehm.com>
2025-01-13 17:48:27 +0100

    Change all_benchmarks, use lazy dataframes
    Not really changing much, but was more of an exercise in using polars.

--- src/nik_graphs/aggregate/all_benchmarks.py
+++ src/nik_graphs/aggregate/all_benchmarks.py
@@ -5,22 +5,23 @@ def deplist(dispatch=None):
 def aggregate_path(path, outfile=None):
     import polars as pl
 
-    df_low, df_high = [pl.read_parquet(f) for f in deplist()]
+    df_low, df_high = [pl.scan_parquet(f) for f in deplist()]
+    colnames = df_high.collect_schema().names() + ["dim"]
+
+    def sorted_df(df: pl.DataFrame):
+        return df
 
     df_low1 = df_low.with_columns(
         pl.lit(None, dtype=float).alias("learned_temp"),
         pl.col("name").alias("run_name"),
         pl.lit(2).alias("dim"),
-    )
-    sortcol = sorted(df_low1.columns)
-    df_low2 = df_low1.select(sortcol)
+    ).select(colnames)
 
-    df_high1 = df_high.with_columns(pl.lit(128).alias("dim"))
-    df_high2 = df_high1.select(sortcol)
+    df_high1 = df_high.with_columns(pl.lit(128).alias("dim")).select(colnames)
 
-    df = pl.concat((df_low2, df_high2))
+    df = pl.concat((df_low1, df_high1))
 
     if outfile is not None:
-        df.write_parquet(outfile)
+        df.sink_parquet(outfile)
 
     return df
