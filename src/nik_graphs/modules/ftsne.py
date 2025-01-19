02360440a41879f57a6f617a7f563fe260fc8491
Niklas Böhm <mail@jnboehm.com>
2025-01-19 23:09:54 +0100

    Fix feature tsne to evaluate on all embeddings

--- src/nik_graphs/modules/ftsne.py
+++ src/nik_graphs/modules/ftsne.py
@@ -7,6 +7,7 @@ import openTSNE.callbacks
 from scipy import sparse
 from sklearn import preprocessing
 
+from ..graph_utils import make_adj_mat
 from ..path_utils import path_to_kwargs
 from .tsne import TSNECallback, tsne
 
@@ -14,18 +15,16 @@ __partition__ = "cpu-galvani"
 
 
 def run_path(path, outfile):
-    from ..graph_utils import make_adj_mat
 
     zipf = path.parent / "1.zip"
 
     with open(path / "files.dep", "a") as f:
-        pyobjs = [path_to_kwargs, tsne]
+        pyobjs = [make_adj_mat, path_to_kwargs, tsne]
         [f.write(inspect.getfile(x) + "\n") for x in pyobjs]
 
     name, kwargs = path_to_kwargs(path)
     assert name == "ftsne"
-
-    rng = np.random.default_rng(kwargs.get("random_state", 5015153))
+    do_all = kwargs.pop("all", False)
 
     # A = sparse.load_npz(zipf)
     npz = np.load(zipf)
@@ -35,21 +34,6 @@ def run_path(path, outfile):
     else:
         features = npz["features"]
         data_zip = zipf
-
-    if features.shape[1] > 50:
-        from sklearn import decomposition
-
-        pca = decomposition.PCA(50, random_state=rng.integers(2**31 - 1))
-        X = pca.fit_transform(features)
-    else:
-        X = features
-
-    A, _ = make_adj_mat(X, seed=rng.integers(2**31 - 1))
-
-    callbacks_every_iters = kwargs.get("callbacks_every_iters", 10)
-    callbacks = TSNECallback(outfile, callbacks_every_iters, save_freq=5)
-    kwargs["callbacks_every_iters"] = callbacks_every_iters
-    kwargs["callbacks"] = callbacks
 
     sig = inspect.signature(tsne)
     default_init = sig.parameters["initialization"].default
@@ -63,42 +47,39 @@ def run_path(path, outfile):
         Y_init /= Y_init[:, 0].std() / 1e-4
         kwargs["initialization"] = Y_init
 
-    Y = tsne(A, **kwargs)
+    Y = feature_tsne(features, **kwargs)
+
+    if do_all:
+        tsned = tsne_other_embeddings(zipf.parent, **kwargs)
 
     with zipfile.ZipFile(outfile, "a") as zf:
         with zf.open("embedding.npy", "w") as f:
             np.save(f, Y)
 
+        if do_all:
+            for k, v in tsned.items():
+                with zf.open(f"{k}.npy", "w") as f:
+                    np.save(f, v)
 
-def tsne(
-    A,
-    n_epochs=750,
-    early_exaggeration_iter=None,
-    n_jobs=-1,
-    initialization="spectral",
-    random_state=505**3,
-    row_norm=True,
-    negative_gradient_method="fft",
-    **kwargs,
-):
-    if early_exaggeration_iter is None:
-        n_iter = n_epochs * 2 // 3
-        early_exaggeration_iter = n_epochs // 3
+
+def feature_tsne(features, random_state=5015153, **kwargs):
+    rng = np.random.default_rng(random_state)
+
+    if features.shape[1] > 50:
+        from sklearn import decomposition
+
+        pca = decomposition.PCA(50, random_state=rng.integers(2**31 - 1))
+        X = pca.fit_transform(features)
     else:
-        n_iter = n_epochs
-    tsne = openTSNE.TSNE(
-        n_jobs=n_jobs,
-        n_iter=n_iter,
-        early_exaggeration_iter=early_exaggeration_iter,
-        initialization=initialization,
-        random_state=random_state,
-        negative_gradient_method=negative_gradient_method,
-        **kwargs,
-    )
+        X = features
 
-    # normalize affinities row-wise, then symmetrize.  Will be
-    # normalized into a joint probability distribution by
-    # `PrecomputedAffinities`
-    P = preprocessing.normalize(A, norm="l1", axis=1) if row_norm else A
-    affinities = openTSNE.affinity.PrecomputedAffinities((P + P.T) / 2)
-    return tsne.fit(affinities=affinities)
+    A, _ = make_adj_mat(X, seed=rng.integers(2**31 - 1))
+
+    return tsne(A, **kwargs)
+
+
+def tsne_other_embeddings(embeddings_dir, **kwargs):
+    npz = np.load(embeddings_dir / "1.zip")
+    other_embks = [k for k in npz.keys() if k.startswith("embeddings/step-")]
+    tsnes = [feature_tsne(npz[k], **kwargs) for k in other_embks]
+    return {k: t for k, t in zip(other_embks, tsnes)}
