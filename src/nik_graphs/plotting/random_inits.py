49bcd504d0c0d8957e3e994b8c8b628a8a23a25d
Niklas Böhm <mail@jnboehm.com>
2025-01-22 17:32:30 +0100

    Fix/Change random_inits to only show a subset of plots

--- src/nik_graphs/plotting/random_inits.py
+++ src/nik_graphs/plotting/random_inits.py
@@ -3,90 +3,94 @@ from pathlib import Path
 
 def deplist(dispatch: Path):
     assert dispatch.name == "random_inits"
-    return ["../dataframes/random_inits.h5", "table/dataset_info.parquet"]
+    return ["../dataframes/all_layouts.h5", "../dataframes/random_inits.h5"]
 
 
 def plot_path(plotname, outfile, format="pdf"):
     import h5py
-    import polars as pl
 
     deps = deplist(plotname)
-    h5file = deps[0]
-    df = pl.read_parquet(deps[1])
+    h5afile = deps[0]
+    h5bfile = deps[1]
 
-    with h5py.File(h5file) as h5:
-        return plot(h5, df, outfile=outfile, format=format)
+    with h5py.File(h5afile) as h5a, h5py.File(h5bfile) as h5b:
+        return plot(h5a, h5b, outfile=outfile, format=format)
 
 
-def plot(h5, df, outfile, format="pdf"):
+def plot(h5a, h5b, outfile, *, datasets=["computer", "photo"], format="pdf"):
+    import itertools
+
     import matplotlib as mpl
     import matplotlib.patheffects
     import numpy as np
     from matplotlib import pyplot as plt
     from scipy import linalg
 
-    from ..plot import translate_acc_short, translate_plotname
+    from ..plot import letter_dict, translate_acc_short, translate_plotname
 
-    dataset_keys = [k for k in df.sort("n_edges")["dataset_key"] if k in h5]
-    fig = plt.figure(
-        figsize=(3.35, 1 * len(dataset_keys)),
+    fig, axxs = plt.subplots(
+        len(datasets),
+        2,
+        figsize=(3.5, 3.75),
         constrained_layout=dict(h_pad=0, w_pad=0),
     )
-    figs = fig.subfigures(len(dataset_keys))
-    for i, (sfig, dataset) in enumerate(zip(figs, dataset_keys)):
-        sfig.supylabel(
-            translate_plotname(dataset),
-            fontsize=plt.rcParams["axes.titlesize"],
-        )
-        h5_ds = h5[dataset]
-        keys = "tsne fa2 tfdp".split()
-        labels = h5_ds["labels"]
-        row = h5_ds["edges/row"]
-        col = h5_ds["edges/col"]
+    ltrdict = letter_dict()
+    ltrdict.update(horizontalalignment="left")
 
-        axd = sfig.subplot_mosaic([keys])
-        anchor = h5_ds["tsne"]  # take any array
+    inits = dict(default=h5a, random=h5b)
+    for (dataset, (initstr, h5)), ltr, ax in zip(
+        itertools.product(datasets, inits.items()), "abcdef", axxs.flat
+    ):
+        labels = h5a[dataset]["labels"]
+        row = h5a[dataset]["edges/row"]
+        col = h5a[dataset]["edges/col"]
+
+        anchor = h5a[dataset]["tsne"]  # take any array
         # this is the same order as in low_dim_metrics.py
-        for key, ax in axd.items():
-            ax = axd[key]
-            ax.set_title(translate_plotname(key)) if i == 0 else None
 
-            data = np.array(h5_ds[key])
-            rot, _scale = linalg.orthogonal_procrustes(data, anchor)
-            data = data @ rot.round(10)
-            ax.scatter(*data.T, c=labels, rasterized=True)
-            ax.axis("equal")
-            ax.set_axis_off()
+        tstr = f"{translate_plotname(dataset)}, {initstr} init."
+        ax.set_title(tstr)
 
-            lines = (
-                f"{translate_acc_short(k)}$ = ${v:5.1%}"
-                for k, v in h5_ds[key].attrs.items()
-                if k != "lin"
-            )
-            txt = "\n".join(sorted(lines, key=len, reverse=True))
-            p_eff = [
-                mpl.patheffects.withStroke(linewidth=1.1, foreground="white")
-            ]
-            ax.text(
-                1,
-                1,
-                txt,
-                transform=ax.transAxes,
-                fontsize=5,
-                ha="right",
-                va="top",
-                path_effects=p_eff,
-            )
+        data = np.array(h5[dataset]["tsne"])
+        rot, _scale = linalg.orthogonal_procrustes(data, anchor)
+        data = data @ rot.round(10)
+        ax.scatter(*data.T, c=labels, rasterized=True)
+        ax.axis("equal")
+        ax.set_axis_off()
 
-            pts = np.hstack((data[row], data[col])).reshape(len(row), 2, 2)
-            lines = mpl.collections.LineCollection(
-                pts,
-                alpha=0.05,
-                color="xkcd:dark grey",
-                antialiaseds=True,
-                zorder=0.9,
-                rasterized=True,
-            )
-            ax.add_collection(lines)
+        lines = (
+            f"{translate_acc_short(k)}$ = ${v:5.1%}".strip()
+            for k, v in h5[dataset]["tsne"].attrs.items()
+            if k != "lin"
+        )
+        txt = "\n".join(sorted(lines, key=len, reverse=True))
+        # import sys
+
+        # print(lines, file=sys.stderr)
+        # print(txt, file=sys.stderr)
+        # p_eff = [mpl.patheffects.withStroke(linewidth=1.1, foreground="white")]
+        ax.text(
+            1,
+            1,
+            txt,
+            transform=ax.transAxes,
+            fontsize=6,
+            ha="right",
+            va="top",
+            ma="right",
+            # path_effects=p_eff,
+        )
+        ax.set_title(ltr, **ltrdict)
+
+        pts = np.hstack((data[row], data[col])).reshape(len(row), 2, 2)
+        lines = mpl.collections.LineCollection(
+            pts,
+            alpha=0.05,
+            color="xkcd:dark grey",
+            antialiaseds=True,
+            zorder=0.9,
+            rasterized=True,
+        )
+        ax.add_collection(lines)
 
     fig.savefig(outfile, format=format, metadata=dict(CreationDate=None))
