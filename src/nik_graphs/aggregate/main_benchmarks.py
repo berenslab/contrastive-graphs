144df2c62611012bbf4489f35204a69cddf9887b
Niklas Böhm <mail@jnboehm.com>
2025-01-23 13:00:50 +0100

    Change main_benchmarks to reflect new df columns
    no run_name column anymore

--- src/nik_graphs/aggregate/main_benchmarks.py
+++ src/nik_graphs/aggregate/main_benchmarks.py
@@ -6,12 +6,7 @@ def aggregate_path(path, outfile=None):
     import polars as pl
 
     df_in = pl.scan_parquet(deplist()[0])
-    df = df_in.filter(
-        ~pl.col("run_name").is_in(["cne", "cne,loss=infonce-temp"])
-    ).select(
-        pl.all().exclude("name"),
-        pl.col("name").replace("CNE, τ=0.05", "graph CNE"),
-    )
+    df = df_in.filter(~pl.col("name").is_in(["cne", "cne,loss=infonce-temp"]))
 
     if outfile is not None:
         df.sink_parquet(outfile)
