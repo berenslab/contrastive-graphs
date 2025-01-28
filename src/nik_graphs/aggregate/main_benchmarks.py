3225c441e84933af9ad3f598ab25c613652dc03a
Niklas Böhm <mail@jnboehm.com>
2025-01-28 14:48:05 +0100

    Select best (on average) node2vec run for main_benchmarks

--- src/nik_graphs/aggregate/main_benchmarks.py
+++ src/nik_graphs/aggregate/main_benchmarks.py
@@ -6,12 +6,31 @@ def aggregate_path(path, outfile=None):
     import polars as pl
 
     df_in = pl.scan_parquet(deplist()[0])
-    df = df_in.filter(
+
+    # get the best p, q parameter combination for node2vec and save
+    # those off in df_pq, so that we can do a semi join on that.  It
+    # also carries along the p, q as nulls so that all other methods
+    # are also preserved
+    df_pq = (
+        df_in.unpivot(
+            ["recall", "knn", "lin"], index=["p", "q", "name", "dataset"]
+        )
+        .group_by(["dataset", "name", "p", "q"])
+        .agg(pl.mean("value"))
+        .filter(pl.col("value") == pl.max("value").over("dataset", "name"))
+    ).drop("value")
+
+    df = df_in.join(
+        df_pq,
+        on=["dataset", "name", "p", "q"],
+        join_nulls=True,
+        how="semi",
+    ).filter(
         (pl.col("name") == "cne,temp=0.05")
         | ~pl.col("name").str.starts_with("cne"),
     )
 
     if outfile is not None:
-        df.sink_parquet(outfile)
+        df.collect().write_parquet(outfile)
 
     return df
