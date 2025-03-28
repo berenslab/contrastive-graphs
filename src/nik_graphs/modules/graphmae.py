Fix scaling for graphmae

src/nik_graphs/modules/graphmae.py
@@ -66,6 +66,6 @@ def graphmae(
 def make_dataset(adj, feat):
     A = adj.tocoo()
     graph = dgl.graph((A.row, A.col))
-    graph.ndata["feat"] = torch.from_numpy(scale_feats(feat))
+    graph.ndata["feat"] = scale_feats(feat)
 
     return graph

src/nik_graphs/modules/graphmae_extra.py
@@ -59,8 +59,7 @@ def preprocess(graph):
 
 def scale_feats(x):
     scaler = StandardScaler()
-    feats = x.numpy()
-    feats = torch.from_numpy(scaler.fit_transform(feats)).float()
+    feats = torch.from_numpy(scaler.fit_transform(x)).float()
     return feats
 
 