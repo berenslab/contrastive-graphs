d6d8762e5f49dafc3eda186b7ca0a35f420bb217
Niklas Böhm <mail@jnboehm.com>
2025-05-14 13:50:45 +0200

    Change add lpred to accuracies table

--- src/nik_graphs/tables/accuracies.py
+++ src/nik_graphs/tables/accuracies.py
@@ -76,7 +76,9 @@ def format_table(dispatch, outfile, format="tex"):
     match format:
         case "tex":
             return tex_table(
-                df, outfile=outfile, metric_keys=["recall", "knn", "lin"]
+                df,
+                outfile=outfile,
+                metric_keys=["recall", "knn", "lin", "lpred"],
             )
         case "md":
             return md_table(df, outfile=outfile)
