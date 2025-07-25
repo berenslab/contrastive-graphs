dc8685e510e8bfb6a507785384bf4054b71a95c3
Niklas Böhm <mail@jnboehm.com>
2025-07-25 23:36:10 +0200

    Remove node2vec and gfeat from all_benchmarks
    Temp fix

--- src/nik_graphs/aggregate/all_benchmarks.py
+++ src/nik_graphs/aggregate/all_benchmarks.py
@@ -1,14 +1,12 @@
 def deplist(dispatch=None):
     return [f"{d}_dim_benchmarks.parquet" for d in ["low", "high"]] + [
-        "node2vec_pq.parquet",
-        "gfeat_benchmarks.parquet",
     ]
 
 
 def aggregate_path(path, outfile=None):
     import polars as pl
 
-    df_low, df_high, df_n2v, df_gfeat = [pl.scan_parquet(f) for f in deplist()]
+    df_low, df_high = [pl.scan_parquet(f) for f in deplist()]
     colnames = df_high.collect_schema().names() + ["dim", "p", "q", "index"]
 
     plnone = pl.lit(None, dtype=float)
@@ -24,38 +22,13 @@ def aggregate_path(path, outfile=None):
     dfix = df_high.with_row_index(
         offset=df_low1.select(pl.len()).collect().item()
     )
-    n2v_ix = (
-        dfix.filter(pl.col("name") == "node2vec")
-        .select("index")
-        .head(1)
-        .collect()
-        .item()
-    )
     df_high1 = (
         dfix.filter(pl.col("name") != "node2vec")
         .with_columns(pl.lit(128).alias("dim"), p=plnone, q=plnone)
         .select(colnames)
     )
 
-    df_n2v1 = df_n2v.with_columns(
-        dim=pl.lit(128),
-        learned_temp=plnone,
-        index=pl.lit(n2v_ix, dtype=pl.UInt32),
-    ).select(colnames)
-
-    df_gfeat1 = df_gfeat.with_columns(
-        dim=plnone.cast(pl.Int32),
-        learned_temp=plnone,
-        index=pl.lit(2**32 - 1, dtype=pl.UInt32),
-        p=plnone,
-        q=plnone,
-    ).select(colnames)
-
-    df = (
-        pl.concat((df_low1, df_high1, df_n2v1, df_gfeat1))
-        .sort("index")
-        .drop("index")
-    )
+    df = pl.concat((df_low1, df_high1)).sort("index").drop("index")
 
     if outfile is not None:
         df.collect().write_parquet(outfile)
