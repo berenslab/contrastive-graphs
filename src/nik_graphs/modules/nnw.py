7cb04e9f7e28fa39d286829aa67e7b3843ba563c
Niklas Böhm <mail@jnboehm.com>
2025-10-13 17:07:16 +0200

    Fix nnw maybe
    Think argsort needs to be reversed

--- src/nik_graphs/modules/nnw.py
+++ src/nik_graphs/modules/nnw.py
@@ -75,7 +75,7 @@ def graph_knn_recall(
     for i in range(test_size):
         neighbor_edges = A_array[A_array[i]].sum(1).astype(float)
         neighbor_edges[i] = 0
-        idxs = np.argsort(neighbor_edges)[:k]
+        idxs = np.argsort(neighbor_edges)[::-1][:k]
         fraction += sum(idx in Z_neighb[i] for idx in idxs)
     fraction /= test_size * k
 
