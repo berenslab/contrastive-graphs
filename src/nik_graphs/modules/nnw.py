df8766da8ad29368437af4282429a7e191d7cd5e
Niklas Böhm <mail@jnboehm.com>
2025-10-13 21:45:59 +0200

    Fix nnw maybe

--- src/nik_graphs/modules/nnw.py
+++ src/nik_graphs/modules/nnw.py
@@ -71,7 +71,7 @@ def graph_knn_recall(
 
     fraction = 0
     for i in range(test_size):
-        neighbor_edges = A[A_array[i]].sum(0)
+        neighbor_edges = A[A_array[i].astype(bool)].sum(1)
         neighbor_edges[test_ind[i]] = 0
         idxs = np.argsort(neighbor_edges)[::-1][:k]
         fraction += sum(idx in Z_neighb[i] for idx in idxs)
